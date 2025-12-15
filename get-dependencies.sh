ae65069530f0cf08abad6d3ddcec774d450ab371
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-14 23:20:55 -0400

    refactor

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,30 +1,18 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-ARCH="$(uname -m)"
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+ARCH=$(uname -m)
 
+echo "Installing package dependencies..."
+echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	base-devel       \
-	curl             \
-	fuse3            \
-	git              \
-	kdenlive         \
-	kio-extras       \
-	libxcb           \
-	libxcursor       \
-	libxi            \
-	libxkbcommon-x11 \
-	libxrandr        \
-	libxtst          \
-	pipewire-audio   \
-	pulseaudio       \
-	pulseaudio-alsa  \
-	qt6ct            \
-	wget             \
-	xorg-server-xvfb \
-	zsync
+	fuse3           \
+	kdenlive        \
+	kio-extras      \
+	pipewire-audio  \
+	pipewire-jack   \
+	qt6ct
 
 if [ "$ARCH" = 'x86_64' ]; then
 		pacman -Syu --noconfirm libva-intel-driver
@@ -32,11 +20,12 @@ fi
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common intel-media-driver-mini
+get-debloated-pkgs --add-common intel-media-driver-mini
 
 # don't let qt6-webengine be bundled
 pacman -Rsndd --noconfirm qt6-webengine
 
-pacman -Q kdenlive | awk '{print $2; exit}' > ~/version
+# Comment this out if you need an AUR package
+#make-aur-package PACKAGENAME
+
+# If the application needs to be manually built that has to be done down here
